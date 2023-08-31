256d83a8ae244cec6b1b1a4508a0526e35e6c674
SJ <godson0218@gmail.com>
2023-08-31 17:47:11 +0900

    day11

--- aaa.py
+++ aaa.py
@@ -1,1 +1,16 @@
-for i in open(0):a,b,c,d,e,f=map(int,i.split());f-=c;e-=b+(f<0);d-=a+(e<0);print(d,e%60,f%60)
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+T = ('Scalene','Isosceles','Equilateral','Invalid')
+while 1:
+    x = -1
+    a,b,c = sorted(map(int,input().split()))
+    if a==b==c==0:
+        break
+    if c<a+b:
+        x+=1
+        if a==b or b==c:
+            x+=1
+            if a==b==c:
+                x+=1            
+    print(T[x])
